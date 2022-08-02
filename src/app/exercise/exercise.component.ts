3732ae87621e2b29301e87fc769d48f318d75008
Kevin Tang <kevin.tang@gmx.ch>
2022-08-02 19:55:06 +0200

    Commit cleaned up version

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -12,19 +12,21 @@ import {ActivatedRoute} from "@angular/router";
 export class ExerciseComponent implements OnInit {
 
   exercise_id: string = "";
-  title = 'angtest and new';
-  url = 'https://xlogo.inf.ethz.ch/test/ville/#/ville-midi'
+  title = 'Testing postMessage API with xlogoOnline';
 
-
-  // private url = 'http://localhost:3000/#/ville-midi?exercise=4a'
+  // baseUrl = 'https://xlogo.inf.ethz.ch/test/ville/#/ville-midi?exercise='
+  // postMessageUrl = 'https://xlogo.inf.ethz.ch'
+  baseUrl = 'http://localhost:3000/#/ville-midi?exercise='
+  postMessageUrl = 'http://localhost:3000'
   safeUrl: SafeUrl;
+
+  // Only for debugging
   messageLog: String = "-------- Angtest - Message log: --------------- \n"
-  ratio = 0.8
 
 
   constructor(private sanitizer: DomSanitizer, private _snackBar: MatSnackBar, private route: ActivatedRoute,
   ) {
-    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl);
   }
 
   cleanUrl(): SafeUrl {
@@ -33,41 +35,45 @@ export class ExerciseComponent implements OnInit {
 
   exerciseToLoad = new FormControl('');
 
-  callVille = () => {
-    console.log(this.exerciseToLoad.value)
-    var childWindow = document.getElementById("xlogo");
-
-    let message = "\n angtest message: " + this.exerciseToLoad.value
-    this.messageLog += "\n Send message to xlogo: " + message + "  |||||  ";
-    // @ts-ignore
-    document.getElementById('xlogo').contentWindow.postMessage("angtest message: " + this.exerciseToLoad.value,"https://xlogo.inf.ethz.ch")
-  }
 
   ngOnInit(): void {
+    // Initialize event listener for incoming postMessage messages from the iframe.
     if (window.addEventListener) {
       window.addEventListener("message", this.receiveMessage, true);
     }
+    // Parse query parameters in url.
     this.route.queryParams
       .subscribe(params => {
-        console.log(params);
         this.exercise_id = params['exercise'];
-        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://xlogo.inf.ethz.ch/test/ville/#/ville-midi?exercise='+this.exercise_id)
+        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.exercise_id)
       });
   }
 
   receiveMessage: any = (event: MessageEvent) => {
-    console.log("Angtest: Receive message from : " );
+    // Only for debugging
+    console.log("Angtest: Receive message from : ");
     console.log("Angtest: Origin: " + event.origin);
     console.log("Angtest: data: " + event.data);
-    this.messageLog += "\n Receive message from : " + event.origin + ". and data: " + event.data +  "  |||||  ";
+    this.messageLog += "\n Receive message from : " + event.origin + ". and data: " + event.data + "  |||||  ";
+    this.messageLog += " Parsed json: " + JSON.parse(event.data);
 
-    if(event.data == "correct"){
+    // Parsing incoming responses from the iframe.
+    let response = (JSON.parse(event.data));
+    if (response["correct"]) {
       //Toast notification on the host application
-      console.log(" Angtest origin: " + event.origin)
-      this._snackBar.open("Student solved exercise correctly", "", {duration: 10000})
-    }else if(event.data == "wrong"){
-      this._snackBar.open("Student solved exercise wrong", "", {duration: 10000})
+      this._snackBar.open("Student solved exercise " + response["name"] + " correctly", "", {duration: 10000})
+    } else {
+      this._snackBar.open("Student solved exercise " + response["name"] + " wrong", "", {duration: 10000})
     }
 
   };
+
+
+  // Only for debugging: Sending message to the xlogoOnline iframe and expecting and echo response
+  testPostMessageWithXLogo = () => {
+    let message = "\n angtest message: " + this.exerciseToLoad.value
+    this.messageLog += "\n Send message to xlogo: " + message;
+    // @ts-ignore
+    document.getElementById('xlogo').contentWindow.postMessage("angtest message: " + this.exerciseToLoad.value, this.postMessageUrl)
+  }
 }
